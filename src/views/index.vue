a7f361eeeb76a56e41318a54c0659fbd8e03633b
王伟通 <zkeduo030@zkeduo.com>
2022-02-10 13:41:33 +0800

    login

---
+++ src/views/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-container class="out">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu :default-openeds="['1', '3']">
+
+          <el-submenu :index="item.index" v-for="item in aside" :key="item.id">
+            <template slot="title"><i class="el-icon-menu"></i>{{item.title}}</template>
+            <el-menu-item :index="item.index+'-'+ite.index" v-for="ite in item.children" :key="ite.id">{{ite.title}}</el-menu-item>
+            <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+          </el-submenu>
+
+
+
+<!-- 
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-menu"></i>首页</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"
+              ><i class="el-icon-menu"></i>权限管理</template
+            >
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-submenu> -->
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      aside: [
+        { id: 0, title: "首页", index: 1, children: [
+            {id: 0, title: "数据统计", index: 1, children: ""},
+          ] 
+        },
+        {
+          id: 1,
+          title: "权限管理",
+          index: 2,
+          children: [
+            { id: 0, title: "会员列表", index: 1, children: "" },
+            { id: 1, title: "会员删除", index: 2, children: "" },
+            { id: 2, title: "权限分类", index: 3, children: "" },
+          ],
+        },
+        { id: 2, title: "创作管理", index: 3, children: "" },
+        { id: 3, title: "系统设置", index: 4, children: "" },
+      ],
+    };
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped lang="scss">
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="out">
+    
+    <div class="box">
+      <div class="head">
+        <img src="../assets/logo.png" alt=""/>
+      </div>
+      
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="success" class="login" @click="login">登录</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      ruleForm: {
+          username: '',
+          password:"",
+      },
+      rules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 1, max: 9, message: '长度在 1 到 9 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+          ],
+      }
+    };
+  },
+  methods: {
+    login(){
+      this.$router.push('index')
+    }
+  },
+  components: {},
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .out{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #B3C0D1;
+    .box{
+      position: relative;
+      width: 500px;
+      height: 350px;
+      margin: 200px 0 0 50%;
+      transform: translate(-50%);
+      background: #fff;
+      padding: 100px 30px 0 0;
+      border-radius: 10px;
+      ::v-deep .el-form-item__label{
+        width: 80px !important;
+      }
+      .login{
+        float: right;
+      }
+      >.head{
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border: 5px solid #222;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%);
+        top: -50px;
+        overflow: hidden;
+        img{
+          width: 75%;
+          height: 75%;
+          margin: 50% 0 0 50%;
+          transform: translate(-50%,-50%);
+        }
+      }
+    }
+  }
+  
+</style>
